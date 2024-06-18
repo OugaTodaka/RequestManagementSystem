d8f86456ecbe42a2093d85fa13d35af8c86d4942
RikutoToyota <fko2347075@stu.o-hara.ac.jp>
2024-06-18 18:41:58 +0900

    一覧

--- Management/views.py
+++ Management/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
-def index(request):
-    return HttpResponse('qawsedrftgyhujikolp;')
+
+def top(request):
+    template = loader.get_template("top.html")
+    return HttpResponse(template.render({}, request))
--- RequestManagementSystem/urls.py
+++ RequestManagementSystem/urls.py
@@ -19,5 +19,5 @@ from django.urls import include,path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("management/",include("Management.urls")),
+    path("",include("Management.urls")),
 ]
