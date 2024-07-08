e52ebb16c518b9befa1e29d36fa997b012adbbc5
RikutoToyota <fko2347075@stu.o-hara.ac.jp>
2024-07-08 17:22:54 +0900

    aaa

--- Management/views.py
+++ Management/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Request,RequestFunction
 
 
 def top(request):
     template = loader.get_template("top.html")
     context = {'user': request.user}
-    return HttpResponse(template.render({}, request))
+    cliente = Request.objects.all()
+    data = {'request': cliente }
+    return HttpResponse(template.render(data, request))
