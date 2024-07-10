c3e6d6853474b57f0a1c148e988bbd5c6e9eb421
OugaTodaka <fko2347017@stu.o-hara.ac.jp>
2024-07-10 17:03:01 +0900

    モデルの編集

--- Management/models.py
+++ Management/models.py
@@ -1,16 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 
 class Request(models.Model):
     request_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100, null=False, blank=False)
-    system_name = models.CharField(max_length=30, null=False, blank=False, default='default')
+    system_name = models.CharField(max_length=100, null=False, blank=False, default='default')
+    system_overview = models.CharField(max_length=1000, null=False, blank=False, default='default')
     mail = models.EmailField(max_length=100, null=False, blank=False)
-    date = models.DateField(null=False, blank=False)
-
-
-class RequestFunction(models.Model):
-    function_id = models.AutoField(primary_key=True)
-    request_id = models.ForeignKey(Request, on_delete=models.CASCADE)
-    function_name = models.CharField(max_length=50, null=False, blank=False)
-    description = models.TextField(max_length=400, null=False, blank=False)
+    date = models.DateField(auto_now_add=True, null=False, blank=False)
+    def __str__(self):
+        return self.system_name
--- Management/views.py
+++ Management/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from .models import Request,RequestFunction
+from .models import Request
 
 
 def top(request):
