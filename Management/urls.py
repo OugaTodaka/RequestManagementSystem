dc7dd1b61902ed0d587a1ebc636881f2c926fdc7
OugaTodaka <fko2347017@stu.o-hara.ac.jp>
2024-07-17 17:23:00 +0900

    url修正

--- Management/urls.py
+++ Management/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'Management'
 
 urlpatterns = [
-    path("top/", views.top, name="top"),
+    path("", views.top, name="top"),
     path("detail/<int:id>",views.detail, name="detail"),
     path("detail/<int:id>/update",views.detail_update,name="detail_update"),
     path("portfolio/",views.portfolio, name="portfolio"),
