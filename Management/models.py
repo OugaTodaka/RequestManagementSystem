8f96e5788bc50cbb0bdc5f57b957a346ca5adfb4
OugaTodaka <fko2347017@stu.o-hara.ac.jp>
2024-09-03 18:29:09 +0900

    model修正

--- Management/models.py
+++ Management/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class Request(models.Model):
@@ -18,3 +19,7 @@ class Request(models.Model):
 
     def __str__(self):
         return self.system_name
+
+class User(AbstractUser):
+    pass
+ 
